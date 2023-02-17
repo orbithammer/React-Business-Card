f1b4b5a76503dfa93cc5bd9be0073015f69042c0
Brock Penner <orbithammer@users.noreply.github.com>
2023-02-18 08:16:51 +0900

    One more time!

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import React from "react"
-// import FontAwesome from "@fortawesome/react-fontawesome"
-// import { FontAwesomeIcon } from "fontawesome"
-// import { FontAwesomeIcon } from "fontawesome"
+
 
 export default function App() {
     return (
         <div>
             <main>
-                <img className="portrait" src="portrait.jpg" />
+                <img className="portrait" src="./images/portrait.png" />
                 <div className="info">
                     <section className="titles">
                         <h1>Brock Penner</h1>
                         <h4 className="job-title">Frontend Developer</h4>
-                        <h4>[INSERT WEBSITE HERE]</h4>
+                        <h4><a onClick={()=>{window.open("https://jovial-sunshine-78a3f0.netlify.app/index.html", "_blank")}}>Click here for portfolio.</a></h4>
                     </section>
                     <section className="contact-info">
-                        <a>Email</a>
-                        <a>LinkedIn</a>
+                        <a onClick={()=>{window.open("mailto: brockpenner@protonmail.com", "_blank")}}><img href="" src="./images/envelope-solid.svg" />Email</a>
+                        <a onClick={()=>{window.open("https://www.linkedin.com/in/brock-penner-97322062/", "_blank")}}><img src="./images/linkedin.svg" className="linkedin-icon"/>LinkedIn</a>
                     </section>
                     <section className="details">
                         <h3>About</h3>
@@ -26,9 +24,19 @@ export default function App() {
                     </section>
                 </div>
                 <footer>
-                    <p>test</p>
+                    <a onClick={()=>{window.open("https://techhub.social/@brockpenner", "_blank")}}>
+                        <img src="./images/mastodon.svg" />
+                    </a>
+                    <a onClick={()=>{window.open("https://www.twitter.com/BrockTIPenner", "_blank")}}>
+                        <img src="./images/twitter.svg" />
+                    </a>
+                    <a onClick={()=>{window.open("https://www.github.com/orbithammer", "_blank")}}>
+                        <img src="./images/github.svg" />
+                    </a>
                 </footer>
             </main>
         </div>
     )
 }
+
+// <a><img src="./images/mastodon.svg" /></a>
