5bfa51eb550a7925dc785606dd157792965dc621
Brock Penner <orbithammer@users.noreply.github.com>
2024-02-26 16:53:23 +0900

    updated img to import

--- App.jsx
+++ App.jsx
@@ -1,20 +1,25 @@
 import React from "react"
-
+import Portrait from "/images/portrait.jpg"
+import Envelope from "/images/envelope-solid.svg"
+import Linkedin from "/images/linkedin.svg"
+import Mastodon from "/images/mastodon.svg"
+import Twitter from "/images/twitter.svg"
+import Github from "/images/github.svg"
 
 export default function App() {
     return (
         <div>
             <main>
-                <img className="portrait" src="./images/portrait.jpg" />
+                <img className="portrait" src={Portrait} />
                 <div className="info">
                     <section className="titles">
                         <h1>Brock Penner</h1>
-                        <h4 className="job-title">Frontend Developer</h4>
+                        <h4 className="jobTitle">Frontend Developer</h4>
                         <h4><a onClick={()=>{window.open("https://jovial-sunshine-78a3f0.netlify.app/index.html", "_blank")}}>Click here for portfolio.</a></h4>
                     </section>
-                    <section className="contact-info">
-                        <a onClick={()=>{window.open("mailto: brockpenner@protonmail.com", "_blank")}}><img href="" src="./images/envelope-solid.svg" />Email</a>
-                        <a onClick={()=>{window.open("https://www.linkedin.com/in/brock-penner-97322062/", "_blank")}}><img src="./images/linkedin.svg" className="linkedin-icon"/>LinkedIn</a>
+                    <section className="contactInfo">
+                        <a onClick={()=>{window.open("mailto: brockpenner@protonmail.com", "_blank")}}><img href="" src={Envelope} />Email</a>
+                        <a onClick={()=>{window.open("https://www.linkedin.com/in/brock-penner-97322062/", "_blank")}}><img src={Linkedin} className="linkedinIcon"/>LinkedIn</a>
                     </section>
                     <section className="details">
                         <h3>About</h3>
@@ -25,18 +30,16 @@ export default function App() {
                 </div>
                 <footer>
                     <a onClick={()=>{window.open("https://techhub.social/@brockpenner", "_blank")}}>
-                        <img src="./images/mastodon.svg" />
+                        <img src={Mastodon} />
                     </a>
                     <a onClick={()=>{window.open("https://www.twitter.com/BrockTIPenner", "_blank")}}>
-                        <img src="./images/twitter.svg" />
+                        <img src={Twitter} />
                     </a>
                     <a onClick={()=>{window.open("https://www.github.com/orbithammer", "_blank")}}>
-                        <img src="./images/github.svg" />
+                        <img src={Github} />
                     </a>
                 </footer>
             </main>
         </div>
     )
 }
-
-// <a><img src="./images/mastodon.svg" /></a>
