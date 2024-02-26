2c762461bbe31609a149559c79fa69cdd399a804
Brock Penner <orbithammer@users.noreply.github.com>
2024-02-26 15:00:12 +0900

    converted to vite
    updated portrait & interests

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <div>
             <main>
-                <img className="portrait" src="./images/portrait.png" />
+                <img className="portrait" src="./images/portrait.jpg" />
                 <div className="info">
                     <section className="titles">
                         <h1>Brock Penner</h1>
@@ -20,7 +20,7 @@ export default function App() {
                         <h3>About</h3>
                         <p>I am a frontend developer that keeps to the KISS design principle. I love to learn anything that will improve my craft and am willing to incorporate <strong><em>any and all</em></strong> feedback.</p>
                         <h3>Interests</h3>
-                        <p>Wordsmith and admirer of master scribes. Rider of alpine waves. Aficionado of art-house cinema and popcorn flicks. Caretaker to repltiles and beetles. Dreamer of a solarpunk future.</p>
+                        <p>Wordsmith and admirer of master scribes. Rider of alpine waves. Aficionado of art-house cinema and popcorn flicks. Dreamer of a solarpunk future.</p>
                     </section>
                 </div>
                 <footer>
